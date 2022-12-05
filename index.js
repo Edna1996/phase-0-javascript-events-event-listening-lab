b6f2200aa1a4b012e0778960ac0ccfc489b772b9
edna1996 <guantaiedna@gmapl.com>
2022-12-05 12:20:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const btn = document.getElementById("button")
+    btn.addEventListener("click",()=>{alert("Hello, I was clicked")})
+    console.log(addingEventListener);
 }
+addingEventListener();
